6314707c0451f8352a1672533c39310ba03a3709
Purushottam Dubey <83211588+MantraTechnologiesLtd@users.noreply.github.com>
2024-09-29 21:14:06 +0100

    Add files via upload

---
+++ ReactCrashCource/DemoState/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Message } from './Components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Message></Message>
+      </div>
+    </div>
+  );
+}
+
+export default App;
